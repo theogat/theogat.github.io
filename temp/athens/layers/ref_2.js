d7d169d76030e7ce885246c87e3d40aa11dcca62
theogat <30630306+theogat@users.noreply.github.com>
2020-11-30 14:34:53 +0200

    Add files via upload

---
+++ temp/athens/layers/ref_2.js
@@ -0,0 +1,8 @@
+var json_ref_2 = {
+"type": "FeatureCollection",
+"name": "ref_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "Reference point" }, "geometry": { "type": "Point", "coordinates": [ 23.718521405791552, 37.973253229755571 ] } }
+]
+}
